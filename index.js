b65211dc6e75e79255fceb5ea4cd0c489785c90c
StratoDev89 <danieltestingemail01@gmail.com>
2023-06-26 14:16:34 -0400

    mod index

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(200).send({ message: "app running ok" });
+  res.status(200).send({ message: "App running OK" });
 });
+app.get("/home", (req, res) => {
+    res.status(200).send({ message: "Home endpoint OK" });
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
