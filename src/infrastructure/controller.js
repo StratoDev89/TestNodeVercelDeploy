e7b217432809d9390c99056693e150248fdffade
StratoDev89 <danieltestingemail01@gmail.com>
2023-06-27 10:52:08 -0400

    add user model and find users controller

--- src/infrastructure/controller.js
+++ src/infrastructure/controller.js
@@ -1,10 +1,13 @@
+const UserModel = require("./user.model");
+
 class UserController {
   getOne(req, res) {
     res.status(200).send({ message: "Get One OK" });
   }
 
-  getAll(req, res) {
-    res.status(200).send({ message: "Get All OK" });
+  async getAll(req, res) {
+    const users = await UserModel.find();
+    res.status(200).send({ message: "Get All OK", users });
   }
 }
 
